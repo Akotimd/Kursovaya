fadca8087122127ed9c3e7f65b80d5a1e2418213
Losker <loskertopgame@gmail.com>
2026-01-21 01:09:37 +0200

    hw13.1

--- Stroki i drugie/src/Main.java
+++ Stroki i drugie/src/Main.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class Main {
     //    задние 1
-    public static void CounterYears(int year) {
+    public static void checkLeapYear(int year) {
         if (year > 1584) {
             if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
                 System.out.println(year + " год является високосным");
@@ -15,22 +15,22 @@ public class Main {
     //задание 2
     public static void suggestAppVersion(int osType, int ClientDeviceYear) {
         int currentYear = LocalDate.now().getYear();
-        if (ClientDeviceYear >= currentYear) {
-            if (osType == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        } else {
+        if (ClientDeviceYear < currentYear) {
             if (osType == 0) {
                 System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
             } else {
                 System.out.println("Установите облегчённую версию приложения для Android по ссылке");
             }
+        } else {
+            if (osType == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         }
     }
 
-    public static int DistanceToDelivery(int deliveryDistance) {
+    public static int distanceToDelivery(int deliveryDistance) {
         int deliveryDays = 1;
         if (deliveryDistance > 100) {
             System.out.println("Доставки нет");
@@ -42,19 +42,19 @@ public class Main {
             } else if (deliveryDistance <= 20) {
                 deliveryDays = 1;
             }
-
             System.out.println("Потребуется дней: " + deliveryDays);
         }
         return deliveryDays;
     }
-        public static void main (String[]args){
+
+    public static void main(String[] args) {
 //        задние 1
-            CounterYears(2021);
+        checkLeapYear(2010);
 //        задание 2
-            suggestAppVersion(1, 2023);
+        suggestAppVersion(0, 2028);
 //        задание 3
-            DistanceToDelivery(101);
-        }
+        distanceToDelivery(101);
     }
+}
 
 
