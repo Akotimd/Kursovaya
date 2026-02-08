d5cb8ad5c7136de344333f2b527de79df60adc84
Losker <loskertopgame@gmail.com>
2026-02-08 21:25:56 +0200

    initial commit

---
+++ Kursovaya/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+            for (int j = 0; j < 0; j++) {
+                System.out.println("j = " + j);
+            }
+        }
+    }
+}
